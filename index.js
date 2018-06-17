aee9b8ec5d2e22d69618a491fe9d25c889ca97c8
Nico Lindemann <nico.lindemann@proux.net>
2018-06-17 19:44:31 +0200

    style: fix comment styles

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const encodeLengthHex = n => { return (n <= 127) ? toHex(n) : toHex(128 + (toHex
 const prepadSigned = hexStr => { return (parseInt(hexStr[0], 16) >= 8 ? '00' : '') + hexStr }
 
 const asn1encode = (type, n) => {
-  if (type === '03') n = '00' + n                 // Special Handling for BIT STRINGS
+  if (type === '03') n = '00' + n // Special Handling for BIT STRINGS
   return type + (n ? encodeLengthHex(n.length / 2) : '') + (n || '00')
 }
 
@@ -15,17 +15,17 @@ module.exports = certificate => {
   }
 
   return Buffer.from(
-    asn1encode('30',                              // SEQUENCE
-      asn1encode('30',                            // SEQUENCE
-        asn1encode('06', '2a864886f70d010101') +  // OBJECT IDENTIFIER
-        asn1encode('05')) +                       // NULL
-      asn1encode('03',                            // BIT STRING
-        asn1encode('30',                          // SEQUENCE
-          asn1encode('02',                        // INTEGER
+    asn1encode('30', // SEQUENCE
+      asn1encode('30', // SEQUENCE
+        asn1encode('06', '2a864886f70d010101') + // OBJECT IDENTIFIER
+        asn1encode('05')) + // NULL
+      asn1encode('03', // BIT STRING
+        asn1encode('30', // SEQUENCE
+          asn1encode('02', // INTEGER
             prepadSigned(
               certificate.modulus
             )) +
-          asn1encode('02',                        // INTEGER
+          asn1encode('02', // INTEGER
             prepadSigned(
               toHex(certificate.exponent)
             ))))), 'hex')
